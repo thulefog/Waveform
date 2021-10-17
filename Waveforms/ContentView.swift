f87aa6972c38932cf1cf4c377386e0acf26e2f8b
JMW <no-reply@aquavit.co>
2021-10-17 10:46:19 -0700

    Initial Commit

---
+++ Waveforms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Waveforms
+//
+//  Created by John Matthew Weston on 10/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Waveforms/WaveformsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveformsApp.swift
+//  Waveforms
+//
+//  Created by John Matthew Weston on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct WaveformsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
